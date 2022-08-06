34048740cf64b7fceceaeac4b2c0d15ebc2a2bfc
Oleg Dub <dubanzai@gmail.com>
2022-08-07 00:19:58 +0300

    add adaptive css

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,64 +1,85 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import InputAdornments from './SmallComp/InputAdornments';
-import Button from '@mui/material/Button';
-import { Grid, Typography } from '@mui/material';
-import { Container } from '@mui/system';
-import { Context } from '..';
-import firebase from 'firebase/compat/app';
-import { useContext } from 'react';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import InputAdornments from "./SmallComp/InputAdornments";
+import Button from "@mui/material/Button";
+import { Grid, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import { Context } from "..";
+import firebase from "firebase/compat/app";
+import { useContext } from "react";
 import hero from "./hero.jpg";
-
-
+import "./WelcomePage.css";
 
 const WelcomePage = () => {
+    const { auth } = useContext(Context);
 
-	const { auth } = useContext(Context)
-	
-	const login = async () => {
-		const provider = new firebase.auth.GoogleAuthProvider()
-		const { user } = await auth.signInWithPopup(provider)
-		console.log(user)
-	}
-	
-	// const hero = require('./hero.png')
-	const vector = require('./Vector.png') 
+    const login = async () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        const { user } = await auth.signInWithPopup(provider);
+        console.log(user);
+    };
 
+    // const hero = require('./hero.png')
+    const vector = require("./Vector.png");
 
-			
-	return (
-	<>
-		<Box
-			height="100vh"
-			display="flex"
-			flexDirection="row"
-			justifyContent="center"
-			alignItems="center"
-		>
-			<img src={hero} alt="" width="50%" style={{ height: "100%", width: "50%" }} />
-				
-			<Container maxWidth="sm">
-          		<Grid container>
-					<Grid item xs={12} textAlign="center">
-						<img src={vector} alt="" />
-             			<Stack spacing={6} marginTop={4}>
-							<Typography variant="h4" component="h4" marginBottom={3} textAlign="center" >
-									<b>Login</b>
-							</Typography>
-							<InputAdornments/>		
-						</Stack>
-						<Stack spacing={6} marginTop={3}></Stack>
-							<Button fullWidth variant="outlined" color="secondary" onClick={login} >
-									Continue with Google
-							</Button>
-						<Stack/>
-           			 </Grid>
-         		 </Grid>
-        	</Container>	
-		</Box>
-    </>
-  );
-}
+    return (
+        <>
+            <Box
+                height="100vh"
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+            >
+                <Grid
+                    className="grid"
+                    style={{
+                        margin: 0,
+                        padding: 0,
+                        position: "relative",
+                        width: "50%",
+                        height: "100%",
+                        overflow: "hidden",
+                    }}
+                >
+                    <img
+                        src={hero}
+                        alt=""
+                        style={{
+                            height: "100%",
+                            width: "100%",
+                            objectFit: "cover",
+                        }}
+                    />
+                </Grid>
+
+                <Grid xl={6} lg={6} md={12} xs={12} container>
+                    <Box style={{ margin: "0 auto", padding: "0 20px" }}>
+                        <Box textAlign="center">
+                            <img src={vector} alt="" />
+                        </Box>
+                        <Typography
+                            variant="h4"
+                            component="h4"
+                            marginBottom={3}
+                            textAlign="center"
+                        >
+                            <b>Login</b>
+                        </Typography>
+                        <InputAdornments />
+                        <Button
+                            fullWidth
+                            variant="outlined"
+                            color="secondary"
+                            onClick={login}
+                        >
+                            Continue with Google
+                        </Button>
+                    </Box>
+                </Grid>
+            </Box>
+        </>
+    );
+};
 
 export default WelcomePage;
